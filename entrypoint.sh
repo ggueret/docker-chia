37ac418454c137e359f7b5ff1dcbe1ca90d3c4bc
Geoffrey GUERET <g.gueret@gmail.com>
2021-05-17 04:11:07 +0200

    added harvester role

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 set -e
 
-chia --root-path=/etc/chia/mainnet init
+if [ -d /ca ]; then
+    # Creates a new certificate signed by your main machine's CA
+    chia --root-path=/etc/chia/mainnet init -c /ca
+else
+    chia --root-path=/etc/chia/mainnet init
+fi
 
 if [ -r /mnemonic.key ]; then
     chia --root-path=/etc/chia/mainnet keys add -f /mnemonic.key
@@ -9,4 +14,19 @@ else
     chia --root-path=/etc/chia/mainnet keys generate
 fi
 
-exec chia --root-path=/etc/chia/mainnet "$@"
+sed -i 's/localhost/127.0.0.1/g' /etc/chia/mainnet/config/config.yaml
+
+if [ -n "$ROLE" ]; then
+    if [ "$ROLE" = "harvester" ]; then
+        if [ -n "$FARMER_ADDRESS" ] && [ -n "$FARMER_PORT" ]; then
+            chia configure --set-farmer-peer $FARMER_ADDRESS:$FARMER_PORT
+        fi
+        chia --root-path=/etc/chia/mainnet start harvester
+        while true; do sleep 30; done;
+    else
+        echo "unknown role."
+        exit 127
+    fi
+else
+    exec chia --root-path=/etc/chia/mainnet "$@"
+fi
