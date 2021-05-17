2c5993b69dbeeb0086ae96ab8b725ba62d3d7def
Geoffrey GUERET <g.gueret@gmail.com>
2021-05-17 04:25:03 +0200

    used /etc/chia for set-farmer-peer

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,7 @@ sed -i 's/localhost/127.0.0.1/g' /etc/chia/mainnet/config/config.yaml
 if [ -n "$ROLE" ]; then
     if [ "$ROLE" = "harvester" ]; then
         if [ -n "$FARMER_ADDRESS" ] && [ -n "$FARMER_PORT" ]; then
-            chia configure --set-farmer-peer $FARMER_ADDRESS:$FARMER_PORT
+            chia --root-path=/etc/chia/mainnet configure --set-farmer-peer $FARMER_ADDRESS:$FARMER_PORT
         fi
         chia --root-path=/etc/chia/mainnet start harvester
         while true; do sleep 30; done;
